9c6754e37d082a0ccc4f814570158e519a0afe53
Chai Chaoweeraprasit <wchao1115@hotmail.com>
2025-05-17 00:00:23 +0000

    Make session works properly with requested action, observation, and reward spaces

--- examples/xac_cli/actor_critic_dxc.py
+++ examples/xac_cli/actor_critic_dxc.py
@@ -6,7 +6,7 @@ from compiler_gym.util.logging import init_logging
 def main():
     init_logging(level=logging.DEBUG)
     register_xac_env()
-    with gym.make("xac-v0") as env:
+    with gym.make("xac-v0", observation_space="isa_metrics", reward_space="occupancy") as env:
         env.reset()
         for _ in range(1):
             observation, reward, done, info = env.step(env.action_space.sample())
